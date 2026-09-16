3c3787f6b7496c127f1577c84589466b87981774
Hamin Jeong <causeless3t@naver.com>
2026-09-16 15:18:11 +0900

    소켓 구현 리팩토링

--- Assets/Deploy/Runtime/BaseSocket.cs
+++ Assets/Deploy/Runtime/BaseSocket.cs
@@ -1,256 +1,423 @@
 using System;
 using System.Buffers;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using UnityEngine;
 
 namespace Causeless3t.Network
 {
-	public interface IBuffer
-	{
-		public byte[] Array { get; }
-		public int Count { get; }
-		public int Offset { get; set; }
-	}
-	
-    public abstract class BaseSocket
+    public interface ISocketBuffer
     {
-	    private static readonly int DefaultSendBufferSize    = 8192;  // 소켓 내부 버퍼사이즈(보내기)
-	    private static readonly int DefaultReceiveBufferSize = 65535; // 소켓 내부 버퍼사이즈(받기)
-	    private static readonly int ReadBufferSizeAtOnce     = 8192;  // 한번에 읽을 수 있는 버퍼사이즈
+        byte[] Buffer { get; }
+        int Length { get; }
+    }
 
-	    #region Events
-	    /// <summary>
-	    /// 연결이 수립되면 호출되는 이벤트
-	    /// </summary>
-	    public event Action OnConnected;
-	    /// <summary>
-	    /// byte[] 메시지를 수신할때마다 호출되는 이벤트
-	    /// </summary>
-	    protected event Action<byte[]> OnReceivedBytes;
-	    /// <summary>
-	    /// 소켓관련 Exception이 발생하면 호출되는 이벤트
-	    /// </summary>
-	    public event Action<SocketException> OnSocketException;
-	    /// <summary>
-	    /// 소켓이 Disconnect 될때 발생되는 이벤트
-	    /// </summary>
-	    public event Action OnDisconnected;
-	    #endregion Events
+    public abstract class BaseSocket : IDisposable
+    {
+        private const int DefaultSendBufferSize = 8192;
+        private const int DefaultReceiveBufferSize = 65535;
+        private const int ReadBufferSizeAtOnce = 8192;
 
-	    #region Properties
-	    /// <summary>
-	    /// 내부 소켓 연결 상태
-	    /// </summary>
-	    public abstract bool IsConnected { get; }
-	    #endregion Properties
+        #region Events
 
-	    #region Variables
-	    protected Socket InternalSocket;
-	    private Thread _listenerThread;
-	    private Thread _senderThread;
-	    protected readonly Queue<IBuffer> SendMessages = new();
-	    
-	    protected int SendBufferSize;
-	    protected int ReceiveBufferSize;
-	    protected int ListenerThreadSleepTime = 1;
-	    protected int SenderThreadSleepTime   = 10;
-	    #endregion Variables
+        /// <summary>
+        /// 연결이 수립되면 호출됩니다.
+        /// Worker Thread에서 호출될 수 있습니다.
+        /// </summary>
+        public event Action OnConnected;
 
-	    private static IPAddress GetIP4Address(string address)
-		{
-			var addresses = Dns.GetHostAddresses(address);
-			IPAddress selected = null;
-			for (int index = 0; index < addresses.Length; ++index)
-			{
-				IPAddress ipAddress = addresses[index];
-				if (ipAddress is not { AddressFamily: AddressFamily.InterNetwork }) continue;
+        /// <summary>
+        /// SocketException이 발생하면 호출됩니다.
+        /// Worker Thread에서 호출될 수 있습니다.
+        /// </summary>
+        public event Action<SocketException> OnSocketException;
 
-				if (selected == null)
-					selected = ipAddress;
-				else if (ipAddress.ToString().Equals(address))
-					selected = ipAddress;
-			}
-			return selected;
-		}
+        /// <summary>
+        /// 연결이 종료되면 호출됩니다.
+        /// Worker Thread에서 호출될 수 있습니다.
+        /// </summary>
+        public event Action OnDisconnected;
 
-		/// <summary>
-		/// 지정된 버퍼 사이즈로 소켓을 생성합니다. 기본사이즈는 보내기 8192, 받기 65535입니다.
-		/// </summary>
-		/// <param name="sendBuffer">최대 보내기 버퍼 사이즈</param>
-		/// <param name="receiveBuffer">최대 받기 버퍼 사이즈</param>
-		protected BaseSocket(int sendBuffer = -1, int receiveBuffer = -1)
-		{
-			if (sendBuffer == -1)
-				SendBufferSize = DefaultSendBufferSize;
-			if (receiveBuffer == -1)
-				ReceiveBufferSize = DefaultReceiveBufferSize;
-		}
-		
-		#region Public Functions
-		/// <summary>
-		/// 주어진 주소와 포트로 연결을 시도합니다.
-		/// </summary>
-		/// <param name="address">네트워크 주소</param>
-		/// <param name="port">포트</param>
-		/// <param name="type">프로토콜 방식. 기본은 TCP</param>
-		public virtual void Connect(string address, int port, ProtocolType type = ProtocolType.Tcp)
-		{
-			IPAddress  ipAddress = GetIP4Address(address);
-			IPEndPoint endPoint  = new IPEndPoint(ipAddress, port);
-			if (type != ProtocolType.Tcp && type != ProtocolType.Udp)
-			{
-				Debug.LogError($"{type.ToString()} is unsupported Type! Cannot connected remote server.");
-				return;
-			}
+        #endregion
 
-			if (type == ProtocolType.Tcp)
-				InternalSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, type);
-			else
-				InternalSocket = new Socket(ipAddress.AddressFamily, SocketType.Dgram, type);
+        #region Properties
 
-			try
-			{
-				InternalSocket.BeginConnect(endPoint, OnConnect, null);
-			}
-			catch (Exception e)
-			{
-				Debug.LogError($"Error while accepting connection: {e.Message}\n{e.StackTrace}");
-			}
-		}
-		
-		/// <summary>
-		/// 연결을 중단합니다.
-		/// </summary>
-		public virtual void Stop()
-		{
-			SendMessages!.Clear();
-			if (_senderThread is { IsAlive: true })
-			{
-				_senderThread.Abort();
-				_senderThread = null;
-			}
-			if (_listenerThread is { IsAlive: true })
-			{
-				_listenerThread.Abort();
-				_listenerThread = null;
-			}
+        public bool IsConnected => InternalSocket?.Connected ?? false;
 
-			try
-			{
-				if (InternalSocket?.Connected ?? false)
-					InternalSocket?.Disconnect(false);
-				InternalSocket?.Close();
-				InternalSocket?.Dispose();
-			}
-			catch (ObjectDisposedException e)
-			{
-				Debug.Log($"{e.Message}\n{e.StackTrace}");
-			}
-		}
-		#endregion Public Functions
+        #endregion
 
-		private void OnConnect(IAsyncResult ar)
-		{
-			if (InternalSocket.Connected == false || ar == null)
-				return;
+        #region Fields
 
-			InternalSocket.EndConnect(ar);
+        protected Socket InternalSocket;
 
-			_senderThread = new Thread(SenderWork);
-			_senderThread.Start();
-			_listenerThread = new Thread(ListenerWork);
-			_listenerThread.Start();
+        protected readonly ConcurrentQueue<ISocketBuffer> SendMessages = new();
 
-			OnConnected?.Invoke();
-		}
+        protected readonly int SendBufferSize;
+        protected readonly int ReceiveBufferSize;
 
-		#region Threads
-		private void SenderWork()
-		{
-			while (!IsConnected)
-				Thread.Yield();
-			while (IsConnected)
-			{
-				try
-				{
-					if (InternalSocket == null) break;
-					while (SendMessages.TryDequeue(out var context))
-					{
-						if (context == null) continue;
-						InternalSocket.Send(context.Array!, SocketFlags.None);
-					}
-					Thread.Sleep(SenderThreadSleepTime);
-				}
-				catch (SocketException sock_exc)
-				{
-					Debug.LogError($"{sock_exc.Message}\n{sock_exc.StackTrace}");
-					OnSocketException?.Invoke(sock_exc);
-				}
-				catch (ThreadAbortException thread_exc)
-				{
-					Debug.Log($"{thread_exc.Message}\n{thread_exc.StackTrace}");
-				}
-				catch (Exception exc)
-				{
-					Debug.LogError($"{exc.Message}\n{exc.StackTrace}");
-				}
-			}
-		}
-		
-		private void ListenerWork()
-		{
-			while (!IsConnected)
-				Thread.Yield();
-			while (IsConnected)
-			{
-				try
-				{
-					if (InternalSocket == null) break;
-					var readData = ArrayPool<byte>.Shared.Rent(ReadBufferSizeAtOnce);
-					var nRecv = InternalSocket.Receive(readData, 0, readData.Length, SocketFlags.None);
-					if (nRecv == 0)
-					{
-						ArrayPool<byte>.Shared.Return(readData);
-						Thread.Sleep(ListenerThreadSleepTime);
-						continue;
-					}
-					
-					OnReceivedBytes?.Invoke(readData);
-					ArrayPool<byte>.Shared.Return(readData);
-					Thread.Sleep(ListenerThreadSleepTime);
-				}
-				catch (SocketException sock_exc)
-				{
-					Debug.LogError($"{sock_exc.Message}\n{sock_exc.StackTrace}");
-					OnSocketException?.Invoke(sock_exc);
-				}
-				catch (ThreadAbortException thread_exc)
-				{
-					Debug.Log($"{thread_exc.Message}\n{thread_exc.StackTrace}");
-				}
-				catch (Exception exc)
-				{
-					Debug.LogError($"{exc.Message}\n{exc.StackTrace}");
-				}
-			}
-			OnDisconnected?.Invoke();
-		}
-		#endregion Threads
-		
-		protected bool CanSend(IBuffer context)
-		{
-			if (context is not { Count: > 0 }) return false;
-			if (IsConnected == false)
-			{
-				Debug.LogError("Cannot send message. It was disconnected.");
-				return false;
-			}
-			return true;
-		}
+        private Thread _listenerThread;
+        private Thread _senderThread;
 
-		protected abstract bool Send(IBuffer context);
+        private volatile bool _running;
+        private int _disconnectedRaised;
+
+        #endregion
+
+        protected BaseSocket(int sendBuffer = -1, int receiveBuffer = -1)
+        {
+            SendBufferSize = sendBuffer > 0 ? sendBuffer : DefaultSendBufferSize;
+
+            ReceiveBufferSize = receiveBuffer > 0 ? receiveBuffer : DefaultReceiveBufferSize;
+        }
+
+        #region Public
+
+        /// <summary>
+        /// 지정한 주소와 포트로 연결합니다.
+        /// </summary>
+        public virtual void Connect(string address, int port, ProtocolType type = ProtocolType.Tcp)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+                throw new ArgumentException("Address cannot be null or empty.", nameof(address));
+
+            if (port is <= 0 or > 65535)
+                throw new ArgumentOutOfRangeException(nameof(port));
+
+            if (type != ProtocolType.Tcp && type != ProtocolType.Udp)
+                throw new NotSupportedException($"{type} is not supported.");
+
+            Stop();
+
+            var ipAddress = GetIPv4Address(address);
+            if (ipAddress == null)
+                throw new SocketException((int)SocketError.HostNotFound);
+
+            var endPoint = new IPEndPoint(ipAddress, port);
+
+            InternalSocket = type == ProtocolType.Tcp
+                ? new Socket(ipAddress.AddressFamily, SocketType.Stream, type)
+                : new Socket(ipAddress.AddressFamily, SocketType.Dgram, type);
+
+            InternalSocket.SendBufferSize = SendBufferSize;
+            InternalSocket.ReceiveBufferSize = ReceiveBufferSize;
+
+            try
+            {
+                InternalSocket.BeginConnect(endPoint, OnConnect, null);
+            }
+            catch
+            {
+                DisposeSocket();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 연결을 종료하고 Worker Thread를 정리합니다.
+        /// </summary>
+        public virtual void Stop()
+        {
+            var wasRunning = _running || InternalSocket != null;
+
+            _running = false;
+
+            while (SendMessages.TryDequeue(out _))
+            {
+            }
+
+            ShutdownSocket();
+
+            JoinThread(_senderThread);
+            JoinThread(_listenerThread);
+
+            _senderThread = null;
+            _listenerThread = null;
+
+            DisposeSocket();
+
+            if (wasRunning)
+                RaiseDisconnected();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region Protected
+
+        /// <summary>
+        /// 전송할 버퍼를 Send Queue에 등록합니다.
+        /// </summary>
+        protected bool Send(ISocketBuffer context)
+        {
+            if (!CanSend(context))
+                return false;
+
+            SendMessages.Enqueue(context);
+            return true;
+        }
+
+        /// <summary>
+        /// Socket에서 수신한 데이터입니다.
+        ///
+        /// buffer는 ArrayPool에서 임대한 배열이므로 이 메서드가 반환된 이후
+        /// 참조를 보관해서는 안 됩니다.
+        /// 필요한 경우 데이터를 복사해서 사용해야 합니다.
+        /// </summary>
+        protected abstract void OnReceive(byte[] buffer, int offset, int count);
+
+        protected bool CanSend(ISocketBuffer context)
+        {
+            if (context == null)
+                return false;
+
+            if (context.Buffer == null ||
+                context.Length <= 0 ||
+                context.Length > context.Buffer.Length)
+            {
+                return false;
+            }
+
+            return _running && IsConnected;
+        }
+
+        #endregion
+
+        #region Connection
+
+        private void OnConnect(IAsyncResult asyncResult)
+        {
+            try
+            {
+                var socket = InternalSocket;
+                if (socket == null)
+                    return;
+
+                socket.EndConnect(asyncResult);
+
+                if (!socket.Connected)
+                    return;
+
+                _disconnectedRaised = 0;
+                _running = true;
+
+                _senderThread = new Thread(SenderWork)
+                {
+                    IsBackground = true,
+                    Name = $"{GetType().Name}.Sender"
+                };
+
+                _listenerThread = new Thread(ListenerWork)
+                {
+                    IsBackground = true,
+                    Name = $"{GetType().Name}.Listener"
+                };
+
+                _senderThread.Start();
+                _listenerThread.Start();
+
+                OnConnected?.Invoke();
+            }
+            catch (SocketException exception)
+            {
+                OnSocketException?.Invoke(exception);
+                Stop();
+            }
+            catch
+            {
+                Stop();
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Workers
+
+        private void SenderWork()
+        {
+            try
+            {
+                while (_running)
+                {
+                    if (!SendMessages.TryDequeue(out var context))
+                    {
+                        Thread.Sleep(1);
+                        continue;
+                    }
+
+                    SendAll(context);
+                }
+            }
+            catch (SocketException exception)
+            {
+                if (_running)
+                    OnSocketException?.Invoke(exception);
+            }
+            finally
+            {
+                HandleWorkerStopped();
+            }
+        }
+
+        private void ListenerWork()
+        {
+            var readBuffer = ArrayPool<byte>.Shared.Rent(ReadBufferSizeAtOnce);
+
+            try
+            {
+                while (_running)
+                {
+                    var socket = InternalSocket;
+                    if (socket == null)
+                        break;
+
+                    var received = socket.Receive(readBuffer, 0, ReadBufferSizeAtOnce, SocketFlags.None);
+
+                    // TCP에서 Receive() == 0은 원격지의 정상적인 연결 종료를 의미한다.
+                    if (received == 0)
+                        break;
+
+                    OnReceive(readBuffer, 0, received);
+                }
+            }
+            catch (SocketException exception)
+            {
+                if (_running)
+                    OnSocketException?.Invoke(exception);
+            }
+            catch (ObjectDisposedException)
+            {
+                // Stop()에 의해 Socket이 종료된 경우.
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(readBuffer);
+
+                HandleWorkerStopped();
+            }
+        }
+
+        private void SendAll(ISocketBuffer context)
+        {
+            var socket = InternalSocket;
+            if (socket == null)
+                return;
+
+            var offset = 0;
+            var remaining = context.Length;
+
+            while (_running && remaining > 0)
+            {
+                var sent = socket.Send(context.Buffer, offset, remaining, SocketFlags.None);
+
+                if (sent <= 0)
+                    throw new SocketException((int)SocketError.ConnectionReset);
+
+                offset += sent;
+                remaining -= sent;
+            }
+        }
+
+        private void HandleWorkerStopped()
+        {
+            if (!_running)
+                return;
+
+            _running = false;
+
+            ShutdownSocket();
+            RaiseDisconnected();
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static IPAddress GetIPv4Address(string address)
+        {
+            if (IPAddress.TryParse(address, out var parsedAddress) &&
+                parsedAddress.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return parsedAddress;
+            }
+
+            var addresses = Dns.GetHostAddresses(address);
+
+            foreach (var ipAddress in addresses)
+            {
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+                    return ipAddress;
+            }
+
+            return null;
+        }
+
+        private void ShutdownSocket()
+        {
+            var socket = InternalSocket;
+            if (socket == null)
+                return;
+
+            try
+            {
+                if (socket.Connected)
+                    socket.Shutdown(SocketShutdown.Both);
+            }
+            catch (SocketException)
+            {
+                // 이미 연결이 끊어진 경우 무시합니다.
+            }
+            catch (ObjectDisposedException)
+            {
+                // 이미 정리된 Socket입니다.
+            }
+        }
+
+        private void DisposeSocket()
+        {
+            var socket = InternalSocket;
+            InternalSocket = null;
+
+            if (socket == null)
+                return;
+
+            try
+            {
+                socket.Close();
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+
+            socket.Dispose();
+        }
+
+        private static void JoinThread(Thread thread)
+        {
+            if (thread == null ||
+                !thread.IsAlive ||
+                thread == Thread.CurrentThread)
+            {
+                return;
+            }
+
+            thread.Join();
+        }
+
+        private void RaiseDisconnected()
+        {
+            if (Interlocked.Exchange(ref _disconnectedRaised, 1) != 0)
+                return;
+
+            OnDisconnected?.Invoke();
+        }
+
+        #endregion
     }
 }
--- Assets/Samples/Socket.cs
+++ Assets/Samples/Socket.cs
@@ -1,127 +1,195 @@
 using System;
+using System.Buffers.Binary;
 using System.Net.Sockets;
 
 namespace Causeless3t.Network.Sample
 {
     public sealed class Socket : BaseSocket
     {
-	    private struct MessageBuffer : IBuffer
-	    {
-		    public byte[] Array { get; }
-		    public int Count { get; }
-		    public int Offset { get; set; }
-		    public MessageBuffer(int count)
-		    {
-			    Array = new byte[count];
-			    Count = count;
-			    Offset = 0;
-		    }
-	    }
-	    
-        private struct ReceiveContext
-		{
-			public int Command;
-			public ArraySegment<byte> Message;
-		}
+        private const int LengthFieldSize = sizeof(int);
+        private const int CommandFieldSize = sizeof(int);
+        private const int HeaderSize = LengthFieldSize + CommandFieldSize;
 
-		private static ReceiveContext _convertedContext;
+        private const int DefaultPacketTimeout = 30000;
+        private const int InitialReceiveBufferSize = 8192;
 
-		/// <summary>
-		/// 파싱된 메시지를 넘겨주는 이벤트.
-		/// </summary>
-		public event Action<int, ArraySegment<byte>> OnPacketReceived;
-		
-		private static readonly int PacketTimeout = 30000;
-		
-		/// <summary>
-		/// 지정된 버퍼 사이즈로 소켓을 생성합니다. 기본사이즈는 보내기 8192, 받기 65535입니다.
-		/// </summary>
-		/// <param name="sendsize">보내기 최대 버퍼 사이즈</param>
-		/// <param name="receivesize">받기 최대 버퍼 사이즈</param>
-		public Socket(int sendsize = -1, int receivesize = -1) : base(sendsize, receivesize) { }
-		
-		public override bool IsConnected => InternalSocket?.Connected ?? false;
+        private sealed class MessageBuffer : ISocketBuffer
+        {
+            public byte[] Buffer { get; }
+            public int Length { get; }
 
-		#region Public Functions
-		/// <summary>
-		/// 지정된 주소, 포트로 연결을 수립합니다. 기본 타임아웃은 10초로 설정되어 있습니다.
-		/// </summary>
-		/// <param name="address">네트워크 주소</param>
-		/// <param name="port">포트</param>
-		/// <param name="type">프로토콜 방식. 기본은 TCP</param>
-		public override void Connect(string address, int port, ProtocolType type = ProtocolType.Tcp)
-		{
-			OnReceivedBytes += ReceiveProcess;
-			
-			base.Connect(address, port, type);
-			InternalSocket!.ReceiveTimeout   = InternalSocket.SendTimeout = PacketTimeout; // default : 0(unlimited)
-			InternalSocket.SendBufferSize    = SendBufferSize;                             // default : 8192
-			InternalSocket.ReceiveBufferSize = ReceiveBufferSize;                          // default : 65535
-		}
+            public MessageBuffer(byte[] buffer)
+            {
+                Buffer = buffer ?? throw new ArgumentNullException(nameof(buffer));
+                Length = buffer.Length;
+            }
+        }
 
-		/// <summary>
-		/// 내부 소켓의 타임아웃 시간을 정합니다.
-		/// </summary>
-		/// <param name="time">타임아웃 시간(ms)</param>
-		public void SetTimeout(int time)
-		{
-			if (InternalSocket == null) return;
-			InternalSocket.ReceiveTimeout = InternalSocket.SendTimeout = time;
-		}
+        private byte[] _receiveBuffer = new byte[InitialReceiveBufferSize];
+        private int _receivedLength;
 
-		/// <summary>
-		/// 연결을 중단합니다.
-		/// </summary>
-		public override void Stop()
-		{
-			OnReceivedBytes -= ReceiveProcess;
-			base.Stop();
-		}
-		
-		/// <summary>
-		/// 메시지를 보냅니다.
-		/// </summary>
-		/// <param name="command">커맨드</param>
-		/// <param name="buffer">페이로드</param>
-		/// <returns></returns>
-		public bool SendMessage(int command, byte[] buffer)
-		{
-			MessageBuffer dst = new MessageBuffer(buffer.Length + 4);
-			Buffer.BlockCopy(BitConverter.GetBytes(command), 0, dst.Array, dst.Offset, 4);
-			if (BitConverter.IsLittleEndian)
-				Array.Reverse((Array) dst.Array, dst.Offset, 4);
-			dst.Offset += 4;
-			Buffer.BlockCopy(buffer, 0, dst.Array, dst.Offset, buffer.Length);
-			return Send(dst);
-		}
-		#endregion Public Functions
-		
-		private void ReceiveProcess(byte[] context)
-		{
-			if (context.Length == 0) return;
-			
-			int srcOffset = 0;
-			if (BitConverter.IsLittleEndian)
-				Array.Reverse((Array) context, srcOffset, 4);
-			var command = BitConverter.ToInt32(context, srcOffset);
-			srcOffset += 4;
-			
-			int count = context.Length - srcOffset;
-			var payload = new ArraySegment<byte>(context, srcOffset, count);
-			
-			_convertedContext.Command   = command;
-			_convertedContext.Message   = payload;
-			
-			OnPacketReceived?.Invoke(_convertedContext.Command, _convertedContext.Message);
-		}
+        /// <summary>
+        /// 하나의 완전한 패킷이 파싱되었을 때 호출됩니다.
+        ///
+        /// payload는 내부 Receive Buffer를 참조하므로
+        /// 이벤트 호출 이후 보관하려면 복사해야 합니다.
+        /// </summary>
+        public event Action<int, ArraySegment<byte>> OnPacketReceived;
 
-		protected override bool Send(IBuffer context)
-		{
-			var result = CanSend(context);
-			if (!result) return false;
-			SendMessages!.Enqueue(context);
-			return true;
-		}
+        public Socket(int sendBuffer = -1, int receiveBuffer = -1) : base(sendBuffer, receiveBuffer)
+        {
+        }
+
+        #region Public
+
+        public override void Connect(string address, int port, ProtocolType type = ProtocolType.Tcp)
+        {
+            base.Connect(address, port, type);
+
+            if (InternalSocket != null)
+            {
+                InternalSocket.ReceiveTimeout = DefaultPacketTimeout;
+                InternalSocket.SendTimeout = DefaultPacketTimeout;
+            }
+        }
+
+        public void SetTimeout(int milliseconds)
+        {
+            if (milliseconds < 0)
+                throw new ArgumentOutOfRangeException(nameof(milliseconds));
+
+            var socket = InternalSocket;
+            if (socket == null)
+                return;
+
+            socket.ReceiveTimeout = milliseconds;
+            socket.SendTimeout = milliseconds;
+        }
+
+        /// <summary>
+        /// [Length][Command][Payload] 형식으로 패킷을 전송합니다.
+        ///
+        /// Length에는 Command + Payload의 크기가 기록됩니다.
+        /// 모든 정수는 Network Byte Order(Big Endian)를 사용합니다.
+        /// </summary>
+        public bool SendMessage(int command, byte[] payload)
+        {
+            payload ??= Array.Empty<byte>();
+
+            var bodyLength = CommandFieldSize + payload.Length;
+            var packetLength = LengthFieldSize + bodyLength;
+
+            var packet = new byte[packetLength];
+
+            BinaryPrimitives.WriteInt32BigEndian(packet.AsSpan(0, LengthFieldSize), bodyLength);
+
+            BinaryPrimitives.WriteInt32BigEndian(
+                packet.AsSpan(LengthFieldSize, CommandFieldSize),
+                command);
+
+            if (payload.Length > 0)
+            {
+                Buffer.BlockCopy(payload, 0, packet, HeaderSize, payload.Length);
+            }
+
+            return Send(new MessageBuffer(packet));
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+
+            _receivedLength = 0;
+        }
+
+        #endregion
+
+        #region Receive
+
+        protected override void OnReceive(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null || count <= 0)
+                return;
+
+            EnsureReceiveBufferCapacity(_receivedLength + count);
+
+            Buffer.BlockCopy(buffer, offset, _receiveBuffer, _receivedLength, count);
+
+            _receivedLength += count;
+
+            ParsePackets();
+        }
+
+        private void ParsePackets()
+        {
+            var readOffset = 0;
+
+            while (true)
+            {
+                var available = _receivedLength - readOffset;
+
+                if (available < HeaderSize)
+                    break;
+
+                var bodyLength = BinaryPrimitives.ReadInt32BigEndian(
+                    _receiveBuffer.AsSpan(
+                        readOffset,
+                        LengthFieldSize));
+
+                if (bodyLength < CommandFieldSize)
+                {
+                    throw new InvalidOperationException(
+                        $"Invalid packet length: {bodyLength}");
+                }
+
+                var packetLength = LengthFieldSize + bodyLength;
+
+                if (available < packetLength)
+                    break;
+
+                var commandOffset = readOffset + LengthFieldSize;
+
+                var command = BinaryPrimitives.ReadInt32BigEndian(
+                    _receiveBuffer.AsSpan(
+                        commandOffset,
+                        CommandFieldSize));
+
+                var payloadOffset = commandOffset + CommandFieldSize;
+                var payloadLength = bodyLength - CommandFieldSize;
+
+                var payload = new ArraySegment<byte>(_receiveBuffer, payloadOffset, payloadLength);
+
+                OnPacketReceived?.Invoke(command, payload);
+
+                readOffset += packetLength;
+            }
+
+            if (readOffset == 0)
+                return;
+
+            var remaining = _receivedLength - readOffset;
+
+            if (remaining > 0)
+            {
+                Buffer.BlockCopy(_receiveBuffer, readOffset, _receiveBuffer, 0, remaining);
+            }
+
+            _receivedLength = remaining;
+        }
+
+        private void EnsureReceiveBufferCapacity(int requiredSize)
+        {
+            if (_receiveBuffer.Length >= requiredSize)
+                return;
+
+            var newSize = _receiveBuffer.Length;
+
+            while (newSize < requiredSize)
+                newSize *= 2;
+
+            Array.Resize(ref _receiveBuffer, newSize);
+        }
+
+        #endregion
     }
 }
-
